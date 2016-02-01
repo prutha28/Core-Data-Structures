6dcc16cea7c17c50ba6a399ebc1c9f2b4abb58f8
prutha28 <prutha.bits@gmail.com>
2016-02-01 17:20:44 -0500

    Created the basic Array ADT

---
+++ SinglyLinkedList.java
@@ -0,0 +1,22 @@
+
+
+public class SinglyLinkedList{
+
+	public Node head ;
+	
+	public SinglyLinkedList(){
+		this.head = null ;
+	}
+
+	public void push( int data ){
+
+	}
+
+	public void insertAtFront( int data ){
+
+	}
+	
+	public void insertAtEnd( int data ){
+		
+	}
+}
