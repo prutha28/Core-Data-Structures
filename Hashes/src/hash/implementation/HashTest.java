0bed779528e29adfc6ef98e13310f1dad6619b51
prutha28 <prutha.bits@gmail.com>
2016-03-01 19:25:41 -0500

    Hash Implementation: Implemented containsKey, containsValue for Hashmap<String, Person>

--- Hashes/src/hash/implementation/HashTest.java
+++ Hashes/src/hash/implementation/HashTest.java
@@ -31,37 +31,76 @@ public class HashTest {
 		}
 
 		System.out.println("The number of elements in the hash map before removing is " + hash.size());
-		
+
 		// removing
 		if(hash.get(lookUpKey) != null){
 			hash.remove(lookUpKey) ;
 			System.out.println("After Removing");
 			hash.print() ;
 		}
-		
+
+
+		// Size
 		System.out.println("The number of elements in teh hash map after removing is " + hash.size());
 
+
+		// Iterating over keys
 		System.out.println();
 		System.out.println("Iterating over the keys");
-		
+
 		for( String key : hash.keySet()){
 			System.out.println("Key : " + key + ", Value : " + hash.get(key));
 		}
-		
+
+		// Iterating over values
 		System.out.println();
 		System.out.println("Iterating over the values");
-		
+
 		for( Person person : hash.values()){
 			System.out.println(person);
 		}
+
+		// Iterating over items.
 		System.out.println();
 		System.out.println("Iterating over the items");
-		
+
 		for( Item item : hash.entries()){
 			System.out.println(item);
 			System.out.println();
 		}
+
+
+		// Check for key
+		lookUpKey = "sansa" ;
+		if( hash.containsKey(lookUpKey)){
+			System.out.println("The key " + lookUpKey + " is present in the hash.");
+		}else{
+			System.out.println("The key " + lookUpKey + " is not present in the hash.");
+		}
+
+		lookUpKey = "robb" ;
+		if( hash.containsKey(lookUpKey)){
+			System.out.println("The key " + lookUpKey + " is present in the hash.");
+		}else{
+			System.out.println("The key " + lookUpKey + " is not present in the hash.");
+		}
+
+		// Check for value
+
+		Person lookUpPerson = new Person("catelyn", "Tully", 30) ;
+		if( hash.containsValue(lookUpPerson)){
+			System.out.println("The key " + lookUpPerson + " is present in the hash.");
+		}else{
+			System.out.println("The key " + lookUpPerson + " is not present in the hash.");
+		}
 		
+
+		lookUpPerson = new Person("arya", "stark", 12) ;
+		if( hash.containsValue(lookUpPerson)){
+			System.out.println("The key " + lookUpPerson + " is present in the hash.");
+		}else{
+			System.out.println("The key " + lookUpPerson + " is not present in the hash.");
+		}
 		
 	}
 }
